86a13a21511f7567694392a67c1228f0799b381c
GoDzM4TT3O <28765699+GoDzM4TT3O@users.noreply.github.com>
2018-08-23 14:27:31 +0200

    Update iSay.py

--- iSay.py
+++ iSay.py
@@ -1,6 +1,6 @@
 # iSay by GoDzM4TT3O, created in Pythonista 3
-# Works with both Python 2 and 3
-import speech, console, time
+# Works with both Python 2 and 3. Only uses 7 lines of code!
+import speech, console
 lang = console.input_alert("iSay by GoDzM4TT3O", "Type a language you'd like your phone to speak in (get a list of languages by typing langs)\nExample:\nif my phone will speak in American English, I will type en-US.")
 if lang == 'langs':
 	console.alert("iSay by GoDzM4TT3O", "Here is a list of languages:\n" + "\n".join(sorted(speech.get_recognition_languages())))
